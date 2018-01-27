8824075d4e4c798eed114a73c7ba24a1402db331
Henrique Szostak <henriqueszostak@gmail.com>
2018-01-27 15:37:29 -0200

    The album app is complete! :D - CardSection and Card components added. - Reusable button componenent added

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,12 +1,72 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text, View, Image, Linking } from 'react-native';
+import Button from './Button';
+import Card from './Card';
+import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+  const { title, artist, thumbnail_image, image, url } = album;
+  const { 
+    thumbnailStyle,
+    imageStyle,
+    headerContentStyle,
+    thumbnailContainerStyle,
+    headerTextStyle
+  } = styles;
+
   return (
-    <View>
-      <Text>{ props.album.title}</Text>
-    </View>
+    <Card>
+
+      <CardSection>
+        <View style={thumbnailContainerStyle}>
+          <Image
+            style={thumbnailStyle}
+            source={{ uri: thumbnail_image }}
+          />
+        </View>
+        <View style={headerContentStyle}>
+          <Text style={headerTextStyle}>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSection>
+
+      <CardSection>
+        <Image style={imageStyle} source={{ uri: image }} />
+      </CardSection>
+
+      <CardSection>
+        <Button onPress={() => Linking.openURL(url)}>
+          Buy Now
+        </Button>
+      </CardSection>
+
+    </Card>
   );
+};
+
+const styles = {
+  headerContentStyle: {
+    justifyContent: 'space-around',
+    flexDirection: 'column'
+  },
+  headerTextStyle: {
+    fontSize: 18
+  },
+  thumbnailStyle: { 
+    width: 50,
+    height: 50
+  },
+  imageStyle: { 
+    width: null,
+    height: 300,
+    flex: 1
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  }
 };
 
 export default AlbumDetail;
