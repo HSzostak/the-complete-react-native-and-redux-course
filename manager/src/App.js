c1a2958888abc9c3c0d4277662a3c86c53b4aa64
Henrique Szostak <henriqueszostak@gmail.com>
2018-02-06 00:27:06 -0200

    First State mapped on Redux. The Email input is mapped on the store now, the password mapping is missing

--- manager/src/App.js
+++ manager/src/App.js
@@ -2,14 +2,28 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyAGepdu8DNdOsppFHHc5Vj5UIf7MmDHAB0',
+      authDomain: 'manager-8605c.firebaseapp.com',
+      databaseURL: 'https://manager-8605c.firebaseio.com',
+      projectId: 'manager-8605c',
+      storageBucket: 'manager-8605c.appspot.com',
+      messagingSenderId: '340304054975'
+    };
+
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
-      <Provider store={createStore()}>
-        <View>
-          <Text>Hello!</Text>
-        </View>
+      <Provider store={createStore(reducers)}>
+        <LoginForm />
       </Provider>
     );
   }
