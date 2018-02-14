191d77517c04ea8fefe9c89115abf73fc01237d2
Henrique Szostak <henriqueszostak@gmail.com>
2018-02-14 16:48:18 -0200

    - Auth reducers and actions created

--- manager/src/components/LoginForm.js
+++ manager/src/components/LoginForm.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { connect } from 'react-redux';
 import { emailChanged, passwordChanged, loginUser } from '../actions';
-import { Card, CardSection, Input, Button } from './common';
+import { Card, CardSection, Input, Button, Spinner } from './common';
 
 class LoginForm extends Component {
   onEmailChange(text) {
@@ -17,6 +17,18 @@ class LoginForm extends Component {
     const { email, password } = this.props;
 
     this.props.loginUser({ email, password });
+  }
+
+  renderButton() {
+    if (this.props.loading) {
+      return <Spinner size="large" />;
+    }
+
+    return (
+      <Button onPress={this.onButtonPress.bind(this)}>
+        Login
+      </Button>
+    );
   }
 
   renderError() {
@@ -56,9 +68,7 @@ class LoginForm extends Component {
         {this.renderError()}
 
         <CardSection>
-          <Button onPress={this.onButtonPress.bind(this)}>
-            Login
-          </Button>
+          {this.renderButton()}
         </CardSection>
       </Card>
     );
@@ -74,11 +84,9 @@ const styles = {
 };
 
 const mapStateToProps = ({ auth }) => {
-  return {
-    email: auth.email,
-    password: auth.password,
-    error: auth.error,
-  };
+  const { email, password, error, loading } = auth;
+
+  return { email, password, error, loading };
 };
 
 export default connect(mapStateToProps, {
